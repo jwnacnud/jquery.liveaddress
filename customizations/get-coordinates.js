556a8f6ebebe9acc2c669d13c64145eae791edba
jwnacnud <jwnacnud@gmail.com>
2013-08-15 13:29:42 -0600

    added example for getting Countyfips

--- customizations/get-coordinates.js
+++ customizations/get-coordinates.js
@@ -11,3 +11,17 @@ liveaddress.on("AddressAccepted", function(event, data, previousHandler)
 		console.log(data.response.chosen.metadata.latitude, data.response.chosen.metadata.longitude);
 	previousHandler(event, data);
 });
+
+/*
+	Get Countyfips for a verified address.
+	
+	Assuming:
+	var liveaddress = $.LiveAddress(...);
+*/
+
+liveaddress.on("AddressAccepted", function(event, data, previousHandler)
+{
+	if (data.response.chosen)
+		$('#Countyfips').val(data.response.chosen.metadata.county_fips);
+	previousHandler(event, data);
+});
